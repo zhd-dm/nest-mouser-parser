1f99a2f62c2c17a3277576c10afc4fcf8cb2c3de
Demyan <dyo171717@gmail.com>
2023-10-15 21:02:57 +0300

    refactor: remove apiKey from environment

--- apps/parser/src/app/interfaces/environment/environment.interface.ts
+++ apps/parser/src/app/interfaces/environment/environment.interface.ts
@@ -2,7 +2,6 @@ export interface EnvironmentVariables {
   PORT: number;
   DATABASE_LOCAL_URL: string;
   MOUSER_DOMAIN: string;
-  MOUSER_API_KEY: string;
   MOUSER_API_V1_URL: string;
   MOUSER_API_V2_URL: string;
 }
--- apps/parser/src/app/modules/mouser-connection/mouser-connection.service.ts
+++ apps/parser/src/app/modules/mouser-connection/mouser-connection.service.ts
@@ -18,7 +18,6 @@ import { MouserApiVersion } from '../../models/api-versions.enum';
 export class MouserConnectionService {
   public readonly apiV1Url = this.configService.get<string>('MOUSER_API_V1_URL');
   public readonly apiV2Url = this.configService.get<string>('MOUSER_API_V2_URL');
-  public readonly apiKey = this.configService.get<string>('MOUSER_API_KEY');
 
   constructor(
     private readonly configService: ConfigService,
