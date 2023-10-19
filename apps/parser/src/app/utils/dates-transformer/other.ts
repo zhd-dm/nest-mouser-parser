42b6635b03e9ce0fb59f3314ab749b55eb0dec8f
Demyan <dyo171717@gmail.com>
2023-10-19 20:00:16 +0300

    feat: added some functions to date-transformer.utils

--- apps/parser/src/app/utils/dates-transformer/other.ts
+++ apps/parser/src/app/utils/dates-transformer/other.ts
@@ -11,3 +11,23 @@ export function getMonthNumber(date: Date): number {
 export function getDayNumber(date: Date): number {
   return date.getDate();
 }
+
+// TODO: return HoursNumber
+export function getHours(date: Date): number {
+  return date.getHours()
+}
+
+// TODO: return MinutesNumber
+export function getMinutes(date: Date): number {
+  return date.getMinutes()
+}
+
+// TODO: return SecondsNumber
+export function getSeconds(date: Date): number {
+  return date.getSeconds()
+}
+
+// TODO: return MillisecondsNumber
+export function getMilliseconds(date: Date): number {
+  return date.getMilliseconds()
+}
--- apps/parser/src/app/utils/dates-transformer/pure-dates.ts
+++ apps/parser/src/app/utils/dates-transformer/pure-dates.ts
@@ -1,17 +1,44 @@
-import { getDayNumber, getMonthNumber, getYearNumber } from './other';
+import {
+  getDayNumber,
+  getHours,
+  getMilliseconds,
+  getMinutes,
+  getMonthNumber,
+  getSeconds,
+  getYearNumber,
+} from './other';
 
-export function getPureDate(date: Date): Date {
+export type GetPureDateOptions = {
+  includeHours?: boolean;
+  includeMinutes?: boolean;
+  includeSeconds?: boolean;
+  includeMilliseconds?: boolean;
+};
+
+export function getPureDate(
+  date: Date,
+  options: GetPureDateOptions = {
+    includeHours: false,
+    includeMinutes: false,
+    includeSeconds: false,
+    includeMilliseconds: false,
+  },
+): Date {
   const year = getYearNumber(date);
   const month = getMonthNumber(date);
   const day = getDayNumber(date);
+  const hours = getHours(date);
+  const minutes = getMinutes(date);
+  const seconds = getSeconds(date);
+  const milliseconds = getMilliseconds(date);
   const pureDate = new Date(date);
 
   pureDate.setFullYear(year);
   pureDate.setMonth(month);
   pureDate.setDate(day);
-  pureDate.setHours(0);
-  pureDate.setMinutes(0);
-  pureDate.setSeconds(0);
-  pureDate.setMilliseconds(0);
+  pureDate.setHours(options.includeHours ? hours : 0);
+  pureDate.setMinutes(options.includeMinutes ? minutes : 0);
+  pureDate.setSeconds(options.includeSeconds ? seconds : 0);
+  pureDate.setMilliseconds(options.includeMilliseconds ? milliseconds : 0);
   return pureDate;
 }
